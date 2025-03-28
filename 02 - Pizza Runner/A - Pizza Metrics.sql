467766360389d4bf1800b4997ea557821f4733a4
p-mndl <philipp.meindl0@gmail.com>
2025-03-28 17:44:16 +0100

    implemented SQL fluff and updated formatting

--- 02 - Pizza Runner/A - Pizza Metrics.sql
+++ 02 - Pizza Runner/A - Pizza Metrics.sql
@@ -1,105 +1,93 @@
 -- How many pizzas were ordered?
-SELECT
-  COUNT(*) AS pizzas_ordered
-FROM CS2.customer_orders;
+SELECT COUNT(*) AS pizzas_ordered
+FROM cs2.customer_orders;
 
 -- How many unique customer orders were made?
-SELECT
-  COUNT(DISTINCT order_id) AS unique_orders
-FROM CS2.customer_orders;
+SELECT COUNT(DISTINCT order_id) AS unique_orders
+FROM cs2.customer_orders;
 
 -- How many successful orders were delivered by each runner?
 SELECT
-    runner_id
-  , COUNT(pickup_time) AS succesful_orders
-FROM CS2.runner_orders ro
+    ro.runner_id,
+    COUNT(ro.pickup_time) AS succesful_orders
+FROM cs2.runner_orders AS ro
 GROUP BY ro.runner_id;
 
 -- How many of each type of pizza was delivered?
 SELECT
-    pizza_id
-  , COUNT(*) AS delivery_count
-FROM CS2.customer_orders co
+    co.pizza_id,
+    COUNT(*) AS delivery_count
+FROM cs2.customer_orders AS co
 GROUP BY co.pizza_id;
 
 -- How many Vegetarian and Meatlovers were ordered by each customer?
 -- first try through group by
 SELECT
-    co.customer_id
-  , pn.pizza_name
-  , COUNT(co.order_id) AS order_count
-FROM CS2.customer_orders co
-LEFT
-  JOIN CS2.pizza_names pn
+    co.customer_id,
+    pn.pizza_name,
+    COUNT(co.order_id) AS order_count
+FROM cs2.customer_orders AS co
+LEFT JOIN cs2.pizza_names AS pn
     ON co.pizza_id = pn.pizza_id
 GROUP BY co.customer_id, pn.pizza_name;
 
 -- 2nd try with count as two different columns
 SELECT
-    customer_orders.customer_id
-  , cnt_vegetarian =
-      COUNT(
+    customer_orders.customer_id,
+    cnt_vegetarian =
+    COUNT(CASE WHEN pizza_names.pizza_name = 'Vegetarian' THEN 1 END),
+    cnt_meatlover = COUNT(
         CASE
-          WHEN pizza_names.pizza_name = 'Vegetarian'
-          THEN 1
-          ELSE NULL
+            WHEN pizza_names.pizza_name = 'Meatlovers' THEN 1
         END
-      )
-  , cnt_meatlover =
-      COUNT(
-        CASE
-          WHEN pizza_names.pizza_name = 'Meatlovers'
-          THEN 1
-          ELSE NULL
-        END
-      )
-FROM CS2.customer_orders
-LEFT
-  JOIN CS2.pizza_names
+    )
+FROM cs2.customer_orders
+LEFT JOIN cs2.pizza_names
     ON customer_orders.pizza_id = pizza_names.pizza_id
 GROUP BY customer_orders.customer_id;
 
 -- What was the maximum number of pizzas delivered in a single order?
 
-WITH pizza_per_order AS(
-  SELECT
-      co.order_id
-    , pizza_count = COUNT(pizza_id)
-    , rank_pizza_count = DENSE_RANK() OVER(ORDER BY COUNT(pizza_id) DESC)
-  FROM CS2.customer_orders co
-  LEFT
-    JOIN CS2.runner_orders ro
-      ON co.order_id = ro.order_id
-  WHERE ro.cancellation IS NULL
-  GROUP BY co.order_id
+WITH pizza_per_order AS (
+    SELECT
+        co.order_id,
+        pizza_count = COUNT(pizza_id),
+        rank_pizza_count = DENSE_RANK() OVER (
+            ORDER BY COUNT(pizza_id) DESC
+        )
+    FROM cs2.customer_orders AS co
+    LEFT JOIN cs2.runner_orders AS ro
+        ON co.order_id = ro.order_id
+    WHERE ro.cancellation IS NULL
+    GROUP BY co.order_id
 )
 
 SELECT
-    order_id
-  , pizza_count
+    order_id,
+    pizza_count
 FROM pizza_per_order
 WHERE rank_pizza_count = 1;
 
--- For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
-WITH extras_exclusions AS(
-  SELECT
-    customer_id
-  , order_id
-  , changes = 
-      CASE
-        WHEN exclusions IS NULL AND extras IS NULL THEN NULL
-        ELSE CONCAT(exclusions, extras)
-      END
-  FROM CS2.customer_orders
+-- For each customer, how many delivered pizzas had at least 1 change
+-- and how many had no changes?
+WITH extras_exclusions AS (
+    SELECT
+        customer_id,
+        order_id,
+        changes =
+        CASE
+            WHEN exclusions IS NULL AND extras IS NULL THEN NULL
+            ELSE CONCAT(exclusions, extras)
+        END
+    FROM cs2.customer_orders
 )
 
 SELECT
-    ee.customer_id
-  , cnt_changes = COUNT(changes)
-  , cnt_no_changes = COUNT(*) - COUNT(changes)
-FROM extras_exclusions ee
-LEFT
-  JOIN CS2.runner_orders ro
+    ee.customer_id,
+    cnt_changes = COUNT(changes),
+    cnt_no_changes = COUNT(*) - COUNT(changes)
+FROM extras_exclusions AS ee
+LEFT JOIN cs2.runner_orders AS ro
     ON ee.order_id = ro.order_id
 WHERE ro.cancellation IS NULL
 GROUP BY customer_id
@@ -107,49 +95,44 @@ ORDER BY customer_id;
 
 -- How many pizzas were delivered that had both exclusions and extras?
 -- komplizierter
-WITH extras_exclusions AS(
-  SELECT
-  order_id
-  , changes = 
-      CASE
-        WHEN exclusions IS NOT NULL AND extras IS NOT NULL THEN 1
-        ELSE NULL
-      END
-  FROM CS2.customer_orders
+WITH extras_exclusions AS (
+    SELECT
+        order_id,
+        changes =
+        CASE
+            WHEN exclusions IS NOT NULL AND extras IS NOT NULL THEN 1
+        END
+    FROM cs2.customer_orders
 )
 
-SELECT
-    cnt_exclusions_extras = COUNT(ee.changes)
-FROM extras_exclusions ee
-LEFT
-  JOIN CS2.runner_orders ro
+SELECT cnt_exclusions_extras = COUNT(ee.changes)
+FROM extras_exclusions AS ee
+LEFT JOIN cs2.runner_orders AS ro
     ON ee.order_id = ro.order_id
 WHERE ro.cancellation IS NULL;
 
 --einfacher
-SELECT
-    cnt_exclusions_extras = COUNT(*)
-FROM CS2.customer_orders co
-LEFT
-  JOIN CS2.runner_orders ro
+SELECT cnt_exclusions_extras = COUNT(*)
+FROM cs2.customer_orders AS co
+LEFT JOIN cs2.runner_orders AS ro
     ON co.order_id = ro.order_id
 WHERE
-  co.exclusions IS NOT NULL
-  AND co.extras IS NOT NULL
-  AND ro.cancellation IS NULL;
+    co.exclusions IS NOT NULL
+    AND co.extras IS NOT NULL
+    AND ro.cancellation IS NULL;
 
 -- What was the total volume of pizzas ordered for each hour of the day?
 
 SELECT
-    hour_of_day = DATEPART(HOUR, co.order_time)
-  , cnt_orders = COUNT(*)
-FROM CS2.customer_orders co
+    hour_of_day = DATEPART(HOUR, co.order_time),
+    cnt_orders = COUNT(*)
+FROM cs2.customer_orders AS co
 GROUP BY DATEPART(HOUR, co.order_time);
 
 -- What was the volume of orders for each day of the week?
 
 SELECT
-    weekday = DATENAME(WEEKDAY, co.order_time)
-  , cnt_orders = COUNT(*)
-FROM CS2.customer_orders co
+    weekday = DATENAME(WEEKDAY, co.order_time),
+    cnt_orders = COUNT(*)
+FROM cs2.customer_orders AS co
 GROUP BY DATENAME(WEEKDAY, co.order_time);
--- 02 - Pizza Runner/C - Ingredient Optimisation.sql
+++ 02 - Pizza Runner/C - Ingredient Optimisation.sql
@@ -1,57 +1,53 @@
 -- 1. What are the standard ingredients for each pizza?
-WITH split_toppings AS(
+WITH split_toppings AS (
     SELECT
-        pr.pizza_id
-      , topping_id = TRIM(s.VALUE)
-  FROM CS2.pizza_recipes pr
-  CROSS APPLY STRING_SPLIT(pr.toppings, ',') s
+        pr.pizza_id,
+        topping_id = TRIM(s.value)
+    FROM cs2.pizza_recipes AS pr
+    CROSS APPLY STRING_SPLIT(pr.toppings, ',') AS s
 )
 
 SELECT
-    pn.pizza_name
-  , toppings = STRING_AGG(pt.topping_name, ', ')
-FROM split_toppings st
-INNER
-  JOIN CS2.pizza_names pn
+    pn.pizza_name,
+    toppings = STRING_AGG(pt.topping_name, ', ')
+FROM split_toppings AS st
+INNER JOIN cs2.pizza_names AS pn
     ON st.pizza_id = pn.pizza_id
-LEFT
-  JOIN CS2.pizza_toppings pt
+LEFT JOIN cs2.pizza_toppings AS pt
     ON st.topping_id = pt.topping_id
 GROUP BY pn.pizza_name;
 
 -- 2. What was the most commonly added extra?
-WITH ordered_extras AS(
-  SELECT
-      co.order_id
-    , extras = TRIM(s.VALUE)
-  FROM CS2.customer_orders co
-  CROSS APPLY STRING_SPLIT(co.extras, ',') s
+WITH ordered_extras AS (
+    SELECT
+        co.order_id,
+        extras = TRIM(s.value)
+    FROM cs2.customer_orders AS co
+    CROSS APPLY STRING_SPLIT(co.extras, ',') AS s
 )
 
 SELECT
-    pt.topping_name
-  , cnt_extra = COUNT(oe.extras)
-FROM ordered_extras oe
-INNER
-  JOIN CS2.pizza_toppings pt
+    pt.topping_name,
+    cnt_extra = COUNT(oe.extras)
+FROM ordered_extras AS oe
+INNER JOIN cs2.pizza_toppings AS pt
     ON oe.extras = pt.topping_id
 GROUP BY pt.topping_name;
 
 -- 3. What was the most common exclusion?
-WITH ordered_exclusions AS(
-  SELECT
-      co.order_id
-    , exclusion = TRIM(s.VALUE)
-  FROM CS2.customer_orders co
-  CROSS APPLY STRING_SPLIT(co.exclusions, ',') s
+WITH ordered_exclusions AS (
+    SELECT
+        co.order_id,
+        exclusion = TRIM(s.value)
+    FROM cs2.customer_orders AS co
+    CROSS APPLY STRING_SPLIT(co.exclusions, ',') AS s
 )
 
 SELECT
-    pt.topping_name
-  , cnt_exclusion = COUNT(pt.topping_name)
-FROM ordered_exclusions oe
-INNER
-  JOIN CS2.pizza_toppings pt
+    pt.topping_name,
+    cnt_exclusion = COUNT(pt.topping_name)
+FROM ordered_exclusions AS oe
+INNER JOIN cs2.pizza_toppings AS pt
     ON oe.exclusion = pt.topping_id
 GROUP BY pt.topping_name
 ORDER BY pt.topping_name DESC;
@@ -63,69 +59,66 @@ ORDER BY pt.topping_name DESC;
 --     Meat Lovers - Extra Bacon
 --     Meat Lovers - Exclude Cheese, Bacon - Extra Mushroom, Peppers
 WITH
-customer_order_indexed AS(
+customer_order_indexed AS (
     SELECT
-        item_id = ROW_NUMBER() OVER (ORDER BY co.order_id)
-      , co.order_id
-      , co.pizza_id
-      , co.extras
-      , co.exclusions
-    FROM CS2.customer_orders co
+        co.order_id,
+        co.pizza_id,
+        co.extras,
+        co.exclusions,
+        item_id = ROW_NUMBER() OVER (
+            ORDER BY co.order_id
+        )
+    FROM cs2.customer_orders AS co
 ),
 
-ordered_extras AS(
-  SELECT
-      sq.item_id
-    , extras = STRING_AGG(pt.topping_name, ', ')
-  FROM(
+ordered_extras AS (
     SELECT
-        co.item_id
-      , extras = TRIM(s.VALUE)
-    FROM customer_order_indexed co
-    OUTER APPLY STRING_SPLIT(co.extras, ',') AS s  
-  ) AS sq
-  LEFT
-    JOIN CS2.pizza_toppings pt
-      ON sq.extras = pt.topping_id
-  GROUP BY sq.item_id
+        sq.item_id,
+        extras = STRING_AGG(pt.topping_name, ', ')
+    FROM (
+        SELECT
+            co.item_id,
+            extras = TRIM(s.value)
+        FROM customer_order_indexed AS co
+        OUTER APPLY STRING_SPLIT(co.extras, ',') AS s
+    ) AS sq
+    LEFT JOIN cs2.pizza_toppings AS pt
+        ON sq.extras = pt.topping_id
+    GROUP BY sq.item_id
 ),
 
-ordered_exclusions AS(
-  SELECT
-      sq.item_id
-    , exclusions = STRING_AGG(pt.topping_name, ', ')
-  FROM(
+ordered_exclusions AS (
     SELECT
-        co.item_id
-      , exclusions = TRIM(s.VALUE)
-    FROM customer_order_indexed co
-    CROSS APPLY STRING_SPLIT(co.exclusions, ',') AS s
-  ) AS sq
-  LEFT
-    JOIN CS2.pizza_toppings pt
-      ON sq.exclusions = pt.topping_id
-  GROUP BY sq.item_id
+        sq.item_id,
+        exclusions = STRING_AGG(pt.topping_name, ', ')
+    FROM (
+        SELECT
+            co.item_id,
+            exclusions = TRIM(s.value)
+        FROM customer_order_indexed AS co
+        CROSS APPLY STRING_SPLIT(co.exclusions, ',') AS s
+    ) AS sq
+    LEFT JOIN cs2.pizza_toppings AS pt
+        ON sq.exclusions = pt.topping_id
+    GROUP BY sq.item_id
 )
 
 SELECT
-    pizza = pn.pizza_name +
-    CASE
-      WHEN oextr.extras IS NOT NULL THEN ' - Extra ' + oextr.extras
-      ELSE ''
-    END +
-    CASE
-      WHEN oexcl.exclusions IS NOT NULL THEN ' - Exclude ' + oexcl.exclusions
-      ELSE ''
+    pizza = pn.pizza_name
+    + CASE
+        WHEN oextr.extras IS NOT NULL THEN ' - Extra ' + oextr.extras
+        ELSE ''
     END
-FROM customer_order_indexed co
-LEFT
-  JOIN CS2.pizza_names pn
+    + CASE
+        WHEN oexcl.exclusions IS NOT NULL THEN ' - Exclude ' + oexcl.exclusions
+        ELSE ''
+    END
+FROM customer_order_indexed AS co
+LEFT JOIN cs2.pizza_names AS pn
     ON co.pizza_id = pn.pizza_id
-LEFT
-  JOIN ordered_extras oextr
+LEFT JOIN ordered_extras AS oextr
     ON co.item_id = oextr.item_id
-LEFT
-  JOIN ordered_exclusions oexcl
+LEFT JOIN ordered_exclusions AS oexcl
     ON co.item_id = oexcl.item_id;
 
 -- 5. Generate an alphabetically ordered comma separated ingredient list for 
@@ -133,197 +126,195 @@ LEFT
 -- any relevant ingredients
 --     For example: "Meat Lovers: 2xBacon, Beef, ... , Salami"
 WITH
-customer_order_indexed AS(
+customer_order_indexed AS (
     SELECT
-        item_id = ROW_NUMBER() OVER (ORDER BY co.order_id)
-      , co.order_id
-      , co.pizza_id
-      , co.extras
-      , co.exclusions
-      , pizza_recipe = pr.toppings
-      , pn.pizza_name
-    FROM CS2.customer_orders co
-    LEFT
-      JOIN CS2.pizza_recipes pr
+        co.order_id,
+        co.pizza_id,
+        co.extras,
+        co.exclusions,
+        pizza_recipe = pr.toppings,
+        pn.pizza_name,
+        item_id = ROW_NUMBER() OVER (
+            ORDER BY co.order_id
+        )
+    FROM cs2.customer_orders AS co
+    LEFT JOIN cs2.pizza_recipes AS pr
         ON co.pizza_id = pr.pizza_id
-    LEFT
-      JOIN CS2.pizza_names pn
+    LEFT JOIN cs2.pizza_names AS pn
         ON co.pizza_id = pn.pizza_id
 ),
 
-base_ingredients AS(
-  SELECT
-      sq.item_id
-    , sq.ingredient
-  FROM(
+base_ingredients AS (
     SELECT
-        co.item_id
-      , ingredient = TRIM(s.VALUE)
-    FROM customer_order_indexed co
-    CROSS APPLY STRING_SPLIT(co.pizza_recipe, ',') AS s
-  ) AS sq
+        sq.item_id,
+        sq.ingredient
+    FROM (
+        SELECT
+            co.item_id,
+            ingredient = TRIM(s.value)
+        FROM customer_order_indexed AS co
+        CROSS APPLY STRING_SPLIT(co.pizza_recipe, ',') AS s
+    ) AS sq
 ),
 
-ordered_extras AS(
-  SELECT
-      sq.item_id
-    , ingredient = sq.extras
-  FROM(
+ordered_extras AS (
     SELECT
-        co.item_id
-      , extras = TRIM(s.VALUE)
-    FROM customer_order_indexed co
-    CROSS APPLY STRING_SPLIT(co.extras, ',') AS s  
-  ) AS sq
+        sq.item_id,
+        ingredient = sq.extras
+    FROM (
+        SELECT
+            co.item_id,
+            extras = TRIM(s.value)
+        FROM customer_order_indexed AS co
+        CROSS APPLY STRING_SPLIT(co.extras, ',') AS s
+    ) AS sq
 ),
 
-ordered_exclusions AS(
-  SELECT
-      sq.item_id
-    , ingredient = sq.exclusions
-  FROM(
+ordered_exclusions AS (
     SELECT
-        co.item_id
-      , exclusions = TRIM(s.VALUE)
-    FROM customer_order_indexed co
-    CROSS APPLY STRING_SPLIT(co.exclusions, ',') AS s
-  ) AS sq
+        sq.item_id,
+        ingredient = sq.exclusions
+    FROM (
+        SELECT
+            co.item_id,
+            exclusions = TRIM(s.value)
+        FROM customer_order_indexed AS co
+        CROSS APPLY STRING_SPLIT(co.exclusions, ',') AS s
+    ) AS sq
 ),
 
-combined_ingredients AS(
-  SELECT
-      bi.item_id
-    , bi.ingredient
-  FROM base_ingredients bi
-  EXCEPT
-  SELECT
-      oexcl.item_id
-    , oexcl.ingredient
-  FROM ordered_exclusions oexcl
-  UNION ALL
-  SELECT
-      oextr.item_id
-    , oextr.ingredient
-  FROM ordered_extras oextr
+combined_ingredients AS (
+    SELECT
+        bi.item_id,
+        bi.ingredient
+    FROM base_ingredients AS bi
+    EXCEPT
+    SELECT
+        oexcl.item_id,
+        oexcl.ingredient
+    FROM ordered_exclusions AS oexcl
+    UNION ALL
+    SELECT
+        oextr.item_id,
+        oextr.ingredient
+    FROM ordered_extras AS oextr
 ),
 
-ingredients_cumulated AS(
-  SELECT
-      ci.item_id
-    , pt.topping_name
-    , cnt_topping = COUNT(pt.topping_name)
-  FROM combined_ingredients ci
-  INNER
-    JOIN CS2.pizza_toppings pt
-      ON ci.ingredient = pt.topping_id
-  GROUP BY ci.item_id, pt.topping_name
+ingredients_cumulated AS (
+    SELECT
+        ci.item_id,
+        pt.topping_name,
+        cnt_topping = COUNT(pt.topping_name)
+    FROM combined_ingredients AS ci
+    INNER JOIN cs2.pizza_toppings AS pt
+        ON ci.ingredient = pt.topping_id
+    GROUP BY ci.item_id, pt.topping_name
 ),
 
-order_text AS(
-  SELECT
-      ic.item_id    
-    , recipe = STRING_AGG(
-        coi.pizza_name + ': ' +
-        CASE
-          WHEN ic.cnt_topping >= 2 THEN CAST(ic.cnt_topping AS VARCHAR) + 'x '
-          ELSE ''
-        END
-        + ic.topping_name, ', ')
-  FROM ingredients_cumulated ic
-  INNER
-    JOIN customer_order_indexed coi
-      ON ic.item_id = coi.item_id
-  GROUP BY ic.item_id
+order_text AS (
+    SELECT
+        ic.item_id,
+        recipe = STRING_AGG(
+            coi.pizza_name + ': '
+            + CASE
+                WHEN ic.cnt_topping >= 2
+                    THEN CAST(ic.cnt_topping AS VARCHAR) + 'x '
+                ELSE ''
+            END
+            + ic.topping_name, ', '
+        )
+    FROM ingredients_cumulated AS ic
+    INNER JOIN customer_order_indexed AS coi
+        ON ic.item_id = coi.item_id
+    GROUP BY ic.item_id
 )
 
-SELECT
-    recipe
-FROM order_text;  
+SELECT recipe
+FROM order_text;
 
 
 -- 6. What is the total quantity of each ingredient used in all delivered pizzas
 -- sorted by most frequent first?
 WITH
-customer_order_indexed AS(
+customer_order_indexed AS (
     SELECT
-        item_id = ROW_NUMBER() OVER (ORDER BY co.order_id)
-      , co.order_id
-      , co.pizza_id
-      , co.extras
-      , co.exclusions
-      , pizza_recipe = pr.toppings
-      , pn.pizza_name
-    FROM CS2.customer_orders co
-    LEFT
-      JOIN CS2.pizza_recipes pr
+        co.order_id,
+        co.pizza_id,
+        co.extras,
+        co.exclusions,
+        pizza_recipe = pr.toppings,
+        pn.pizza_name,
+        item_id = ROW_NUMBER() OVER (
+            ORDER BY co.order_id
+        )
+    FROM cs2.customer_orders AS co
+    LEFT JOIN cs2.pizza_recipes AS pr
         ON co.pizza_id = pr.pizza_id
-    LEFT
-      JOIN CS2.pizza_names pn
+    LEFT JOIN cs2.pizza_names AS pn
         ON co.pizza_id = pn.pizza_id
 ),
 
-base_ingredients AS(
-  SELECT
-      sq.item_id
-    , sq.ingredient
-  FROM(
+base_ingredients AS (
     SELECT
-        co.item_id
-      , ingredient = TRIM(s.VALUE)
-    FROM customer_order_indexed co
-    CROSS APPLY STRING_SPLIT(co.pizza_recipe, ',') AS s
-  ) AS sq
+        sq.item_id,
+        sq.ingredient
+    FROM (
+        SELECT
+            co.item_id,
+            ingredient = TRIM(s.value)
+        FROM customer_order_indexed AS co
+        CROSS APPLY STRING_SPLIT(co.pizza_recipe, ',') AS s
+    ) AS sq
 ),
 
-ordered_extras AS(
-  SELECT
-      sq.item_id
-    , ingredient = sq.extras
-  FROM(
+ordered_extras AS (
     SELECT
-        co.item_id
-      , extras = TRIM(s.VALUE)
-    FROM customer_order_indexed co
-    CROSS APPLY STRING_SPLIT(co.extras, ',') AS s  
-  ) AS sq
+        sq.item_id,
+        ingredient = sq.extras
+    FROM (
+        SELECT
+            co.item_id,
+            extras = TRIM(s.value)
+        FROM customer_order_indexed AS co
+        CROSS APPLY STRING_SPLIT(co.extras, ',') AS s
+    ) AS sq
 ),
 
-ordered_exclusions AS(
-  SELECT
-      sq.item_id
-    , ingredient = sq.exclusions
-  FROM(
+ordered_exclusions AS (
     SELECT
-        co.item_id
-      , exclusions = TRIM(s.VALUE)
-    FROM customer_order_indexed co
-    CROSS APPLY STRING_SPLIT(co.exclusions, ',') AS s
-  ) AS sq
+        sq.item_id,
+        ingredient = sq.exclusions
+    FROM (
+        SELECT
+            co.item_id,
+            exclusions = TRIM(s.value)
+        FROM customer_order_indexed AS co
+        CROSS APPLY STRING_SPLIT(co.exclusions, ',') AS s
+    ) AS sq
 ),
 
-combined_ingredients AS(
-  SELECT
-      bi.item_id
-    , bi.ingredient
-  FROM base_ingredients bi
-  EXCEPT
-  SELECT
-      oexcl.item_id
-    , oexcl.ingredient
-  FROM ordered_exclusions oexcl
-  UNION ALL
-  SELECT
-      oextr.item_id
-    , oextr.ingredient
-  FROM ordered_extras oextr
+combined_ingredients AS (
+    SELECT
+        bi.item_id,
+        bi.ingredient
+    FROM base_ingredients AS bi
+    EXCEPT
+    SELECT
+        oexcl.item_id,
+        oexcl.ingredient
+    FROM ordered_exclusions AS oexcl
+    UNION ALL
+    SELECT
+        oextr.item_id,
+        oextr.ingredient
+    FROM ordered_extras AS oextr
 )
 
 SELECT
-    pt.topping_name
-  , cnt_topping = COUNT(pt.topping_name)
-FROM combined_ingredients ci
-INNER
-  JOIN CS2.pizza_toppings pt
+    pt.topping_name,
+    cnt_topping = COUNT(pt.topping_name)
+FROM combined_ingredients AS ci
+INNER JOIN cs2.pizza_toppings AS pt
     ON ci.ingredient = pt.topping_id
 GROUP BY pt.topping_name
 ORDER BY COUNT(pt.topping_name) DESC;
